a7f6626c272dc6511b8b4fff864f6c6caced9d8e
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-08 04:52:17 +0200

    order migration

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,40 @@
 
 namespace App\Models;
 
+use App\Traits\validationTrait;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory,validationTrait;
+
+    static function upsertInstance($data) {
+        $orderCost = self::getOrderTotalCost($data->products_ids);
+        $order = self::updateOrCreate(
+            ['id' => $data->id],
+            [
+                'cost' => $orderCost,
+                'user_id' => $data->user_id ?? Auth::id(),
+                'date' => $data->date ?? Carbon::now(),
+            ]
+        );
+
+        return self::validateResult('success',$order);
+    }
+
+    static function getOrderTotalCost($products_ids) {
+
+        $productPriceSummition = 0;
+
+        foreach($products_ids as $product_id) {
+            $product = Product::find($product_id);
+            $productPriceSummition += $product->price;
+        }
+
+        return $productPriceSummition;
+
+    }
 }
