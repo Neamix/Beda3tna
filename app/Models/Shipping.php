ab6d61597713dca9eba7fe84d1a7b07f39e32097
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-08 04:33:23 +0200

    add validation

--- app/Models/Shipping.php
+++ app/Models/Shipping.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Shipping extends Model
 {
     use HasFactory;
+
+    static function upsertInstance($data) {
+
+    }
+
+    public function deleteInstance() {
+
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
 }
