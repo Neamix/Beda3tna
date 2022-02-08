54f9e10cb6f8f43363e93e40f21d268f539fe99f
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-08 20:45:36 +0200

    Sale crud

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SaleRequest;
+use App\Models\Sale;
 use Illuminate\Http\Request;
 
 class SaleController extends Controller
 {
-    //
+    public function upsert(SaleRequest $request)
+    {
+        return Sale::upsertInstance($request);
+    }
+
+    public function delete(Sale $sale) 
+    {
+        return $sale->deleteInstance();
+    }
 }
--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -2,10 +2,37 @@
 
 namespace App\Models;
 
+use App\Traits\validationTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
-    use HasFactory;
+    use HasFactory,validationTrait;
+
+    protected $guarded = [];
+
+    static function upsertInstance($data) {
+        $sale = self::updateOrCreate(
+                ['id' => $data->id],
+                [
+                    'product_id' => $data->product_id,
+                    'precent'    => $data->precent
+                ]
+            );
+
+        $sale->product = $sale->product()->get();
+
+        return self::validateResult('success',$sale);
+    }
+
+    public function deleteInstance() {
+        $this->delete();
+        return $this;
+    }
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+
 }
