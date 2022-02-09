1f515d6a32c9b5bdad286b963d5a1d0d9105d72e
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-09 10:50:00 +0200

    create image migration

--- database/migrations/2022_02_05_233447_create_images_table.php
+++ database/migrations/2022_02_05_233447_create_images_table.php
@@ -15,7 +15,7 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->morphs('imageable');
+            $table->string('src');
             $table->timestamps();
         });
     }
