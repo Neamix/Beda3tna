6c9449743607c435fa37adc518aee5dc7530af53
Neamix <85620725+Neamix@users.noreply.github.com>
2022-02-09 12:29:25 +0200

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\validationTrait;
+use App\Traits\ValidationTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
